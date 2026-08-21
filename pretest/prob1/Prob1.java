5abcff2fa836b3e4e8087dc72580156097af02cd
Brody Cauthen <bcauth100@uafs.edu>
2026-08-20 19:36:34 -0500

    Prob1

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -1,11 +1,39 @@
 package prob1;
 
+import java.util.Scanner;
+
 public class Prob1 {
 
 	public static void main(String[] args) {
+		int user = 0;
 		
-		System.out.println("Hello world!");
-
+		
+		int[] num = {4, 7, 2, 9, 1};
+		
+		 user = findMaxDifference(num);
+		
+		 System.out.println(user);
+	}
+	
+	public static int findMaxDifference (int[] num) {
+		
+		int max = 0;
+		int test = 0;
+		
+		if(num.length > 2) {
+		for(int i = 0; i < num.length ; i++) {
+			
+			//I just can't remember how to do the adjacent number without going out of bounds
+			test = num[i] - num[i];
+			
+			if(test > max) {
+				max = test;
+			}
+		}
+		}
+		return max;
+		
+		
 	}
 
 }
