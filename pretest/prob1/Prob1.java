25edcf43e0c14b218ce00f31183a1e4fbcfb24dc
Brody Cauthen <bcauth100@uafs.edu>
2026-08-20 19:46:37 -0500

    Updated

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -23,7 +23,7 @@ public class Prob1 {
 		if(num.length > 2) {
 		for(int i = 0; i < num.length ; i++) {
 			
-			//I just couldnt remember how to compare each without going out of bounds ran out of time and such and was overwhelmed/overloaded
+			//I just couldnt remember how to compare each without going out of bounds ran out of time and such and was overwhelmed/overloaded.
 			test = num[i] - num[i];
 			
 			if(test > max) {
